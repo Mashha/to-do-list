892004d21d6a6cfa6f85d402243b775befb7cbff
Masa Orel <orel.masha89@gmail.com>
2023-01-24 15:08:06 +0100

    removed-close icon when you click on all aside el

--- src/index.js
+++ src/index.js
@@ -2,32 +2,48 @@ import "./styles/style.css";
 import { displayPage } from "./dom.js";
 displayPage();
 
-
 const mobileNav = document.querySelector(".mobile-nav");
 const aside = document.querySelector("aside");
-const navIcons = document.querySelectorAll(".icon")
+const navIcons = document.querySelectorAll(".icon");
 
 mobileNav.addEventListener("click", function () {
   aside.classList.toggle("slide-in");
-  navIcons.forEach(icon => {
-    icon.classList.toggle("hide-icon")
-  })
+  removeIcon();
 });
 
 if (screen.width <= 800) {
-  document.querySelector(".today").addEventListener("click", removeAside);
-  document.querySelector(".week-tasks").addEventListener("click", removeAside);
-  document.querySelector(".all-tasks").addEventListener("click", removeAside);
-  document.querySelector(".important").addEventListener("click", removeAside);
+  document.querySelector(".today").addEventListener("click", () => {
+    removeAside();
+    removeIcon();
+  });
+  document.querySelector(".week-tasks").addEventListener("click", () => {
+    removeAside();
+    removeIcon();
+  });
+  document.querySelector(".all-tasks").addEventListener("click", () => {
+    removeAside();
+    removeIcon();
+  });
+  document.querySelector(".important").addEventListener("click", () => {
+    removeAside();
+    removeIcon();
+  });
 
   const projects = document.querySelector(".project-container");
   projects.addEventListener("click", function (e) {
     if (e.target.classList.contains("project-name")) {
       removeAside();
+      removeIcon();
     }
   });
 
   function removeAside() {
     aside.classList.remove("slide-in");
   }
+}
+
+function removeIcon() {
+  navIcons.forEach((icon) => {
+    icon.classList.toggle("hide-icon");
+  });
 }
